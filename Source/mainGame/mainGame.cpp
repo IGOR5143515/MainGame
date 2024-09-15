94f75a508c1b3353070d0bdf58d2003df9e9fbce
IGOR5143515 <122176584+IGOR5143515@users.noreply.github.com>
2024-09-15 17:12:04 +0300

    save

---
+++ Source/mainGame/mainGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "mainGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mainGame, "mainGame" );
